0b96aac938179e9e6b9398d5eeaa6d3ed663de55
rubences <91731294+rubences@users.noreply.github.com>
2025-09-16 18:28:08 +0000

    cmabios

---
+++ app/src/main/java/com/ecocity/app/CalendarActivity.java
@@ -0,0 +1,13 @@
+package com.ecocity.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
---
+++ app/src/main/java/com/ecocity/app/MapActivity.java
@@ -0,0 +1,12 @@
+package com.ecocity.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MapActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
