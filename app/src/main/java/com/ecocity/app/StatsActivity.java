75103256093cb1300646ef0d5ab62012c9c5a221
rubences <91731294+rubences@users.noreply.github.com>
2025-09-16 18:31:24 +0000

    mas coasas

--- app/src/main/java/com/ecocity/app/StatsActivity.java
+++ app/src/main/java/com/ecocity/app/StatsActivity.java
@@ -1,12 +1,39 @@
 package com.ecocity.app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StatsActivity extends AppCompatActivity {
+
+    private int recycleCount = 0;
+    private static final String RECYCLE_COUNT_KEY = "recycleCount";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+
+        if (savedInstanceState != null) {
+            recycleCount = savedInstanceState.getInt(RECYCLE_COUNT_KEY, 0);
+        }
+
+        TextView tvRecycleCount = findViewById(R.id.tvRecycleCount);
+        tvRecycleCount.setText("Bolsas recicladas este mes: " + recycleCount);
+
+        findViewById(R.id.btnAddBag).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recycleCount++;
+                tvRecycleCount.setText("Bolsas recicladas este mes: " + recycleCount);
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(RECYCLE_COUNT_KEY, recycleCount);
     }
 }
